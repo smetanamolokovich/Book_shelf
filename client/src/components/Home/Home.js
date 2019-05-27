eebf2b38a3aea695c819f9ca6b28b81bc4ddd821
Aitu Abdil <aituufs@gmail.com>
2019-05-27 02:14:37 +0200

    Finished home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomeContainer from '../../containers/HomeContainer';
 
 const Home = () => {
     return (
         <div>
-            home
+            <HomeContainer />
         </div>
     );
 };
--- client/src/reducers/books_reducer.js
+++ client/src/reducers/books_reducer.js
@@ -1,6 +1,10 @@
 export default function(state={}, action) {
     switch (action.type) {
-       
+        case 'GET_BOOKS':
+            return {
+                ...state,
+                list: action.payload
+            }
         default:
             return state;
     }
