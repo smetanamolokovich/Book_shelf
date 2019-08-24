5efa2f8b7baab4e0b1d8da1470d31811bcd6f8f9
Aituar Abdil <aituufs@gmail.com>
2019-08-24 22:13:09 +0200

    fixes

---
+++ client/build/precache-manifest.a5bc0a4b9f779a78603a94dd95b24490.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d4124667bd9fbe13ad092a644988d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d5167657f1b3ae1e7c",
+    "url": "/static/js/2.66f32d8c.chunk.js"
+  },
+  {
+    "revision": "6d254e60d70d336c9eed",
+    "url": "/static/js/main.d41d1cf9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
